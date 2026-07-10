877f70d48fa95d6c5388e8f0c36b5f631c3862eb
Ayush <175132680+ayushTripathy-o1@users.noreply.github.com>
2026-07-10 12:33:53 +0530

    feat: update project page

---
+++ src/data/projects.ts
@@ -0,0 +1,14 @@
+import { type Project } from "../types";
+
+const projects: Project[] = [
+    {
+        name: "Multi-Threaded HTTP Server",
+        description: "A multi-threaded HTTP server written in pure Java."
+    },
+    {
+        name: "User Authentication App",
+        description: "A Java Full-Stack User Authentication app with jwt and cookies based authentication."
+    },
+];
+
+export default projects;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,24 @@
 const Projects = () => {
     return (
-        <div className="min-h-screen">Projects</div>
+        <div className="min-h-screen">
+            <div className="flex flex-row items-center justify-between mt-2 mb-0 font-jetbrains">
+                {/* Headings */}
+                <div className="flex flex-col items-center justify-center gap-2 w-64">
+                    <h2 className="underline underline-offset-6 mt-3 mb-4">[ Index ]</h2>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-2 w-64">
+                    <h2 className="underline underline-offset-6 mt-3 mb-4">[ Project Details ]</h2>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-2 w-64">
+                    <h2 className="underline underline-offset-6 mt-3 mb-4">[ Controls ]</h2>
+                </div>
+            </div>
+            <div className="flex flex-row h-screen justify-between mt-2 mb-0 font-jetbrains">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+            </div>
+        </div>
     )
 }
 
